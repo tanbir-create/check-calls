d3b28d244cca36eaae2e34c01234b6d6086029d1
Tanbir Ahmed Mazumdar <46240243+tanbir-create@users.noreply.github.com>
2022-01-18 10:25:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,61 @@
+$('#buttons').on('click', '.btn' , function(){
+    let name = $(this).text();
+    const url = `https://getcalldetails.herokuapp.com/calls/${name}`
+
+    $("#table:not(:first-child)").text('')
+
+   
+
+    fetch(url)
+    .then(response => response.json())
+    .then((results)=>{
+
+        results.det.forEach(result => {
+
+            let cl = result.status;
+            let a = `<div data-cl=${cl} class='all'>
+                        <p>${result.customer_number}</p>
+                        <p>${result.status}</p>
+                        <p>${result.date}</p>
+                        <p>${result.duration}sec</p>
+                     </div>`
+
+            $('#table').append(a);
+        })
+    })
+    
+    $('#deepak').removeClass('active-btn')
+    $('#rosy').removeClass('active-btn')
+    $('#pinkky').removeClass('active-btn')
+
+
+    $(this).addClass('active-btn')
+    
+    
+})
+
+
+$('#category-select').on('change', function(){
+        
+    let category_value = (this).options[this.selectedIndex].value.toLowerCase();
+
+    if(category_value === 'all'){
+        
+        $('[class=all]').show(100);
+    }
+    else{
+       
+            (function(){
+                $('#table>div').filter(function(){
+                    console.log($(this).attr('class'))
+                    return  $(this).attr('data-cl') != category_value;
+                    
+                })
+                .hide();
+            })();
+
+            $("[data-cl=" + category_value + "]").show(100);
+        
+    }
+
+})

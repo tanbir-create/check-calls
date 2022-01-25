ceb6bab078380f9e24824bed49d7aa2c2c06602e
Tanbir Ahmed Mazumdar <46240243+tanbir-create@users.noreply.github.com>
2022-01-25 11:38:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-
-
 let buttonNodes = document.querySelectorAll('.btn')
 let currentActiveBtn;
 let buttons = [...buttonNodes]
+var totalC = 0;
 buttons.forEach(button => {
-
 
 $(button).on('click', function(){
     let name = $(this).text();
@@ -17,14 +15,15 @@ $(button).on('click', function(){
     .then(response => response.json())
     .then((results)=>{
         $("#table:not(:first-child)").text('')
-        $('#table').append(`<div style="padding-bottom: 1.5em;">Total-Calls = ${results.totalCalls} </div>`)
         
+        totalC = results.totalCalls;
+        $('#total-calls').text(`Total Calls = ${totalC}`)
         let sortedDescendeing = results.det.sort((a, b) => new Date(b.date)  - new Date(a.date))
        
         sortedDescendeing.forEach(result => {
             
             let cl = result.status;
-            let a = `<div data-cl=${cl} class='all'>
+            let a = `<div data-cl=${cl} data-date=${result.date} class='all'>
                         <p>${result.customer_number}</p>
                         <p>${result.status}</p>
                         <p>${result.date}</p>
@@ -60,16 +59,18 @@ $('#category-select').on('change', function(){
     if(category_value === 'all'){
         
         $('[class=all]').show(100);
+        $('#total-calls').text(`Total Calls = ${totalC}`)
     }
     else{
        
             (function(){
-                $('#table>div').filter(function(){
+                let a = $('#table>div').filter(function(){
                     
                     return  $(this).attr('data-cl') != category_value;
                     
                 })
-                .hide();
+                $('#total-calls').text(`Total Calls = ${totalC - a.length}`)
+                a.hide();
             })();
 
             $("[data-cl=" + category_value + "]").show(100);
@@ -77,3 +78,29 @@ $('#category-select').on('change', function(){
     }
 
 })
+
+
+$('#find-date').click(function(){
+    let d = $("#date-input").val()
+    console.log($("#date-input").val())
+   
+   
+
+    let category_value = d.toString();
+
+    
+            (function(){
+                let a = $('#table>div').filter(function(){
+                    
+                    return  $(this).attr('data-date') != category_value;
+                    
+                })
+                $('#total-calls').text(`Total Calls = ${totalC - a.length}`)
+                a.hide();
+            })();
+
+            $("[data-date=" + category_value + "]").show(100);
+        
+   
+})
+

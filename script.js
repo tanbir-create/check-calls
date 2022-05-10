47b2b458f0f9837fedcb5f3f4668388d82f6fde5
Tanbir Ahmed Mazumdar <46240243+tanbir-create@users.noreply.github.com>
2022-05-10 12:48:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -105,7 +105,28 @@ $('#find-date').click(function(){
 })
 
 
-let userPhone  =  navigator.userAgent || navigator.vendor || window.opera;
-let x = `<p>${userPhone} dkjfhskv</p>`
+function getMobileOperatingSystem() {
+    var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+
+    // Windows Phone must come first because its UA also contains "Android"
+    if (/windows phone/i.test(userAgent)) {
+        return "Windows Phone";
+    }
+
+    if (/android/i.test(userAgent)) {
+        return "Android";
+    }
+
+    // iOS detection from: http://stackoverflow.com/a/9039885/177710
+    if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+        return "iOS";
+    }
+
+    return "unknown";
+}
+
+// let userPhone   = navigator.userAgent || navigator.vendor || window.opera;
+let x = `<p>${getMobileOperatingSystem()}</p>`
 $("body").append(x);
+
 
